3214d0afe093e4fbd9827d8ac4920ea760d57c53
Ivan Epou <ivanepou92@gmail.com>
2021-11-03 01:40:29 +0300

    Add project folders and header

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Header from "./Components/Header";
+import './assets/css/bootstrap.min.css'
+
+function App() {
+  return (
+    <div className="App">
+        <Header/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import './assets/css/main.css'
+import './assets/css/LineIcons.3.0.css'
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
